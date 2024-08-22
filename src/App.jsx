ee2ddf7fb4c0966f74a24f3ad15a42e119d453a7
Luminix <76751932+Le-Arcanist95@users.noreply.github.com>
2024-08-22 12:31:31 -0400

    Initial commit with pseudo-code project outline of App.js

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+const App = () => {
+    
+    // State for Fetched Meme Array
+    // State for Saved Meme Array
+    // useEffect for axios GET request to "https://api.imgflip.com/get_memes"
+
+    // CRUD Functionality for Saved Memes
+      // PUT - Edit
+      // DELETE
+      // POST - Choice for user adding meme to imgflip for public use.
+
+    return (
+        <div>
+            {/* Header */}
+            {/* Image Display */}
+                {/* Refresh Button */}
+            {/* Creation Form */}
+            {/* Saved List */}
+            {/* Footer */}
+        </div>
+    );
+};
+
+export default App;
