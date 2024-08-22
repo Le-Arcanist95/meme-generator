cf52b0374900642abe03da1edb466e8fbc84b052
Luminix <76751932+Le-Arcanist95@users.noreply.github.com>
2024-08-22 13:10:57 -0400

    Added useState hooks for fetched and saved memes. Restructured comments for personal aesthetic preference.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
+import { useState } from 'react';
+
 const App = () => {
-    
     // State for Fetched Meme Array
+    const [memeList, setMemeList] = useState([]);
     // State for Saved Meme Array
+    const [savedMemeList, setSavedMemeList] = useState([]);
+
     // useEffect for axios GET request to "https://api.imgflip.com/get_memes"
 
     // CRUD Functionality for Saved Memes
-      // PUT - Edit
-      // DELETE
-      // POST - Choice for user adding meme to imgflip for public use.
+    // PUT - Edit
+    // DELETE
+    // POST - Choice for user adding meme to imgflip for public use.
 
     return (
         <div>
-            {/* Header */}
-              {/* Title */}
-              {/* Theme */}
-              {/* Signature */}
-            {/* Meme Generator: Center of page unless displayed with Saved List or Mobile - Then on left side. */}
-              {/* Image Display */}
-                {/* Refresh Button */}
-                {/* Clear Button */}
-                {/* Creation Form */}
-                  {/* Top Text */}
-                  {/* Bottom Text */}
-                  {/* Optional: Additional Input Layouts (For memes with more than 2 slides) */}
-                  {/* Submit Button */}
-            {/* Saved List: Conditionally rendered if user wishes to view - Then on right side*/}
-              {/* List Item */}
-                {/* Edit */}
-                {/* Delete */}
-            {/* Footer */}
+            {/*
+              Header
+                Title
+                Theme
+                Signature
+            */}
+            {/* 
+              Meme Generator: Center of page unless displayed with Saved List or Mobile - Then on left side.
+                Image Display
+                  Refresh
+                  Clear
+                Creation Form
+                  Top/Bottom Inputs
+                  Submit
+                  Optional:
+                    Text Position Sliders
+                    Additional Input Layouts
+            */}
+            {/* 
+              Saved List: Conditionally rendered if user wishes to view - Then on right side
+                List Item
+                  Edit
+                  Delete
+                  Optional: Upload
+            */}
+            {/* 
+              Footer
+                Social Links
+                Accredidation 
+                Copyright
+            */}
         </div>
     );
 };
